ef2f94ab9cf763935c49e52bbac55a2bfe57fdf7
ThreeGold1 <1677001900@qq.com>
2021-07-28 13:17:12 +0800

    Update BranchTest.java

--- src/test/java/org/example/BranchTest.java
+++ src/test/java/org/example/BranchTest.java
@@ -5,5 +5,6 @@ public class BranchTest {
         System.out.println("创建分支");
         System.out.println("主干添加");
         System.out.println("添加分支01");
+        System.out.println("测试更新-添加xcxcxc");
     }
 }
